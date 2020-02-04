9b42141387ac600e2ae4f0af184e857b431aae5a
Arnar Kári Sigurðarson Sandholt <arnarkari.sigurearsonsandholt.9531@student.uu.se>
2020-02-04 11:53:14 +0100

    First version of STAR bash script done

--- scripts/star/star_run.sh
+++ scripts/star/star_run.sh
@@ -1,10 +1,12 @@
 # Script for running the STAR aligner for a set of read files and reference genomes
 
+GROUP_NAME=$(echo ${1} | cut -d '/' -f 3)
+mkdir -p results/star/mapped_reads/${GROUP_NAME}
 
 for f in $(ls ${1} | sed 's/_R._paired.fastq.gz//' | sort -u)
 do
     STAR --genomeDir results/star/index --readFilesIn ${1}/${f}_R1_paired.fastq.gz ${1}/${f}_R2_paired.fastq.gz --readFilesCommand gunzip -c \
-		--runThreadN 16 --outFileNamePrefix results/star/_ --quantMode GeneCounts
+		--runThreadN 16 --outFileNamePrefix results/star/mapped_reads/${GROUP_NAME}/${f}_ --quantMode GeneCounts --outSAMtype BAM SortedByCoordinate
 done
 
 
