0af3d5e04173767b1c208b2f3a1f04464d2bdcfa
Arnar Kári Sigurðarson Sandholt <arnarkari.sigurearsonsandholt.9531@student.uu.se>
2020-02-06 12:48:29 +0100

    Final version of fastqc and trimmomatic wrappers

--- scripts/fastqc/fastqc_wrapper.sh
+++ scripts/fastqc/fastqc_wrapper.sh
@@ -3,7 +3,7 @@
 #SBATCH -A snic2020-15-16
 #SBATCH -p core
 #SBATCH -n 1
-#SBATCH -t 2:00:00
+#SBATCH -t 20:00:00
 #SBATCH -J trimmomatic_run
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user arnarkari.sigurearsonsandholt.9531@student.uu.se
@@ -14,3 +14,6 @@ module load MultiQC/1.8
 
 # Your commands
 bash scripts/fastqc/fastqc_run.sh results/trimmomatic/in_vitro_pilot
+bash scripts/fastqc/fastqc_run.sh results/trimmomatic/in_vitro_complementary
+bash scripts/fastqc/fastqc_run.sh results/trimmomatic/SH-2259
+bash scripts/fastqc/fastqc_run.sh results/trimmomatic/SI-2311
--- scripts/trimmomatic/trimmomatic_wrapper.sh
+++ scripts/trimmomatic/trimmomatic_wrapper.sh
@@ -3,7 +3,7 @@
 #SBATCH -A snic2020-15-16
 #SBATCH -p core
 #SBATCH -n 8
-#SBATCH -t 2:00:00
+#SBATCH -t 20:00:00
 #SBATCH -J trimmomatic_run
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user arnarkari.sigurearsonsandholt.9531@student.uu.se
@@ -12,4 +12,7 @@ module load bioinfo-tools
 module load trimmomatic
 
 # Your commands
-bash scripts/trimmomatic/trimmomatic_run.sh data/in_vitro_pilot/161012_D00457_0163_AC9TWTANXX/Sample_1
+bash scripts/trimmomatic/trimmomatic_run.sh data/in_vitro_pilot/161012_D00457_0163_AC9TWTANXX/Sample_*
+bash scripts/trimmomatic/trimmomatic_run.sh data/in_vitro_complementary/170830_D00457_0216_ACB7DNANXX/Sample_*
+bash scripts/trimmomatic/trimmomatic_run.sh data/SH-2259/191210_A00605_0093_BHNFNHDSXX/Sample_SH-2259-*
+bash scripts/trimmomatic/trimmomatic_run.sh data/SI-2311/191105_A00605_0082_AHGV3LDRXX/Sample_SI-2311-*
