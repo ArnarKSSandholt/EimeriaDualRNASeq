2dff18b60c68ccb088fc87f9cfd81eb756101b45
Arnar Kári Sigurðarson Sandholt <arnarkari.sigurearsonsandholt.9531@student.uu.se>
2020-02-07 15:53:12 +0100

    Fixed minor htseq errors

--- scripts/htseq/htseq_strand_run.sh
+++ scripts/htseq/htseq_strand_run.sh
@@ -8,4 +8,4 @@ for f in $(ls ${1}/*.bam | sed 's/_Aligned.sortedByCoord.out.bam//' | sort -u)
 do
     OUTPUT_NAME=$(echo ${f} | rev | cut -d '/' -f 1 | rev)
     htseq-count -f bam -r pos -s yes -i gene ${f}_Aligned.sortedByCoord.out.bam results/star/merged_reference/eimeria_chicken_merge.gff \
-        > results/htseq/${GROUP_NAME}/${OUTPUT_NAME}.counts
+        > results/htseq/${GROUP_NAME}/${OUTPUT_NAME}_stranded.counts
