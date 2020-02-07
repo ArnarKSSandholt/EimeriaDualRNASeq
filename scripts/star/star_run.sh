89580bd75349d8c58829fcfd280a59fb791a769e
Arnar Kári Sigurðarson Sandholt <arnarkari.sigurearsonsandholt.9531@student.uu.se>
2020-02-07 09:17:50 +0100

    test

--- scripts/star/star_run.sh
+++ scripts/star/star_run.sh
@@ -4,14 +4,15 @@
 GROUP_NAME=$(echo ${1} | cut -d '/' -f 3)
 mkdir -p results/star/mapped_reads/${GROUP_NAME}
 
-for f in $(ls ${1}/*_paired.fastq.gz | sed 's/_R._paired.fastq.gz//' | sort -u)
+for f in $(ls ${1}/*_paired.fastq.gz | sed 's/${1}//' | sed 's/_R._paired.fastq.gz//' | sort -u)
 do
-    STAR --genomeDir /proj/snic2020-16-20/EimeriaDualRNASeq/results/star/index --readFilesIn ${1}/${f}_R1_paired.fastq.gz ${1}/${f}_R2_paired.fastq.gz --readFilesCommand gunzip -c \
-		--runThreadN 8 --outFileNamePrefix results/star/mapped_reads/${GROUP_NAME}/${f}_ --quantMode GeneCounts --outSAMtype BAM SortedByCoordinate
-    STAR --genomeDir /proj/snic2020-16-20/EimeriaDualRNASeq/results/star/index --readFilesIn ${1}/${f}_R1_unpaired.fastq.gz --readFilesCommand gunzip -c \
-		--runThreadN 8 --outFileNamePrefix results/star/mapped_reads/${GROUP_NAME}/${f}_R1_unpaired_ --quantMode GeneCounts --outSAMtype BAM SortedByCoordinate
-    STAR --genomeDir /proj/snic2020-16-20/EimeriaDualRNASeq/results/star/index --readFilesIn ${1}/${f}_R2_unpaired.fastq.gz --readFilesCommand gunzip -c \
-		--runThreadN 8 --outFileNamePrefix results/star/mapped_reads/${GROUP_NAME}/${f}_R2_unpaired_ --quantMode GeneCounts --outSAMtype BAM SortedByCoordinate
+    echo ${f}
+    #STAR --genomeDir /proj/snic2020-16-20/EimeriaDualRNASeq/results/star/index --readFilesIn ${1}/${f}_R1_paired.fastq.gz ${1}/${f}_R2_paired.fastq.gz --readFilesCommand gunzip -c \
+		#--runThreadN 8 --outFileNamePrefix results/star/mapped_reads/${GROUP_NAME}/${f}_ --quantMode GeneCounts --outSAMtype BAM SortedByCoordinate
+    #STAR --genomeDir /proj/snic2020-16-20/EimeriaDualRNASeq/results/star/index --readFilesIn ${1}/${f}_R1_unpaired.fastq.gz --readFilesCommand gunzip -c \
+		#--runThreadN 8 --outFileNamePrefix results/star/mapped_reads/${GROUP_NAME}/${f}_R1_unpaired_ --quantMode GeneCounts --outSAMtype BAM SortedByCoordinate
+    #STAR --genomeDir /proj/snic2020-16-20/EimeriaDualRNASeq/results/star/index --readFilesIn ${1}/${f}_R2_unpaired.fastq.gz --readFilesCommand gunzip -c \
+		#--runThreadN 8 --outFileNamePrefix results/star/mapped_reads/${GROUP_NAME}/${f}_R2_unpaired_ --quantMode GeneCounts --outSAMtype BAM SortedByCoordinate
 done
 
 
