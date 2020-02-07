6039722647478e989ed65d09929800c945d41fd1
Arnar Kári Sigurðarson Sandholt <arnarkari.sigurearsonsandholt.9531@student.uu.se>
2020-02-07 09:09:07 +0100

    Updated the time on the trimmomatic wrapper

--- scripts/trimmomatic/trimmomatic_wrapper.sh
+++ scripts/trimmomatic/trimmomatic_wrapper.sh
@@ -3,7 +3,7 @@
 #SBATCH -A snic2020-15-16
 #SBATCH -p core
 #SBATCH -n 8
-#SBATCH -t 20:00:00
+#SBATCH -t 48:00:00
 #SBATCH -J trimmomatic_run
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user arnarkari.sigurearsonsandholt.9531@student.uu.se
