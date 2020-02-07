9a95d1166dad27295c44c4a82b85df52c55b26c0
Arnar Kári Sigurðarson Sandholt <arnarkari.sigurearsonsandholt.9531@student.uu.se>
2020-02-07 15:51:33 +0100

    Htseq run files and wrapper created

---
+++ scripts/htseq/htseq_wrapper.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+
+#SBATCH -A snic2020-15-16
+#SBATCH -p core
+#SBATCH -n 4
+#SBATCH -t 1:00:00
+#SBATCH -J htseq_run
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user arnarkari.sigurearsonsandholt.9531@student.uu.se
+
+module load bioinfo-tools
+module load htseq/0.9.1
+
+# Your commands
+bash scripts/htseq/htseq_wrapper.sh results/star/mapped_reads/in_vitro_pilot
