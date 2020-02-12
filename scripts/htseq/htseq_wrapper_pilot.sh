2c1b26554bdef93cebb1e491d5dd4c3d8cbfb203
Arnar Kári Sigurðarson Sandholt <arnarkari.sigurearsonsandholt.9531@student.uu.se>
2020-02-12 11:31:26 +0100

    Wrappers for parallel run of Htseq on the main dataset

--- scripts/htseq/htseq_wrapper_pilot.sh
+++ scripts/htseq/htseq_wrapper_pilot.sh
@@ -2,9 +2,9 @@
 
 #SBATCH -A snic2020-15-16
 #SBATCH -p core
-#SBATCH -n 4
-#SBATCH -t 2:00:00
-#SBATCH -J htseq_run
+#SBATCH -n 2
+#SBATCH -t 48:00:00
+#SBATCH -J htseq_run_pilot
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user arnarkari.sigurearsonsandholt.9531@student.uu.se
 
