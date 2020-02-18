d00911b935996f409fd762bfcfae4996c36df714
Arnar Kári Sigurðarson Sandholt <arnarkari.sigurearsonsandholt.9531@student.uu.se>
2020-02-18 10:37:41 +0100

    Finished script for processing htseq output

--- scripts/miscellaneous/read_count_process.py
+++ scripts/miscellaneous/read_count_process.py
@@ -13,5 +13,44 @@ read_count_path = sys.argv[2]
 output_path = sys.argv[3]
 
 read_count_filenames = listdir(metadata_path)
+read_count_filenames.sort()
 metadata_table = pd.read_table(metadata_path)
+metadata_table = metadata_table.sort_values("File_name")
+total_read_num_sum = []
+eimeria_read_num = []
+chicken_read_num = []
+eimeria_read_perc = []
+filenames = []
+old_name = ""
 
+i = 0
+j = 0
+while i < len(read_count_filenames):
+    l_pos = re.search("L00", read_count_filenames[i]).start()
+    curr_name = read_count_filenames[i][0:l_pos-1]
+    if curr_name != old_name:
+        sum_table = pd.read_table(read_count_path+"/"+read_count_filenames[i], header=None, names=["gene_name","gene_count"], index_col=0)
+    else:
+        temp_table = pd.read_table(read_count_path+"/"+read_count_filenames[i], header=None, names=["gene_name","gene_count"], index_col=0)
+        sum_table += temp_table
+        if i+1 == len(read_count_filenames) or not re.search(curr_name, read_count_filenames[i+1]):
+            stat_table = sum_table.iloc[-5:]
+            eimeria_table = sum_table.iloc[-8660:-5]
+            chicken_table = sum_table.iloc[:-8660]
+            total_read_num_sum.append(sum(sum_table.gene_count.iloc[:-5]))
+            eimeria_read_num.append(sum(eimeria_table.gene_count))
+            chicken_read_num.append(sum(chicken_table.gene_count))
+            eimeria_read_perc.append(eimeria_read_num[j]/total_read_num_sum[j])
+            filenames.append(curr_name)
+            stat_table.to_csv(output_path+"/"+curr_name+"_stat_table.csv", sep = ",", index = False)
+            eimeria_table.to_csv(output_path+"/"+curr_name+"_eimeria_table.csv", sep = ",", index = False)
+            chicken_table.to_csv(output_path+"/"+curr_name+"_chicken_table.csv", sep = ",", index = False)
+            j += 1
+    old_name = curr_name
+    i += 1
+
+header_list = ["File_name","Total_number_of_mapped_reads", "Number_of_reads_mapped_to_chicken", "Number_of_reads_mapped_to_Eimeria", "Percentage_of_Eimeria_reads"]
+data_list = [filenames, total_read_num_sum, chicken_read_num, eimeria_read_num, eimeria_read_perc]
+data_table = pd.DataFrame(data_list, columns=header_list)
+out_table = pd.merge(metadata_table,data_table, on = "File_name")
+out_table.to_csv(output_path+"/metadata_table.csv", sep = ",", index = False)
