36199db791fbc7f069587080d38b1ab91138f6bc
Arnar Kári Sigurðarson Sandholt <arnarkari.sigurearsonsandholt.9531@student.uu.se>
2020-02-19 08:54:48 +0100

    Made the run files for the reverse stranded run using the gtf file

--- scripts/htseq/htseq_wrapper_rev_SI-2311.sh
+++ scripts/htseq/htseq_wrapper_rev_SI-2311.sh
@@ -3,8 +3,8 @@
 #SBATCH -A snic2020-15-16
 #SBATCH -p core
 #SBATCH -n 2
-#SBATCH -t 96:00:00
-#SBATCH -J htseq_run_SH-2259
+#SBATCH -t 48:00:00
+#SBATCH -J htseq_run_rev_SI-2311
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user arnarkari.sigurearsonsandholt.9531@student.uu.se
 
@@ -12,4 +12,4 @@ module load bioinfo-tools
 module load htseq/0.9.1
 
 # Your commands
-bash scripts/htseq/htseq_strand_run.sh results/star/mapped_reads/SH-2259
+bash scripts/htseq/htseq_rev_run.sh results/star/mapped_reads/SI-2311
--- scripts/htseq/htseq_wrapper_rev_pilot.sh
+++ scripts/htseq/htseq_wrapper_rev_pilot.sh
@@ -4,7 +4,7 @@
 #SBATCH -p core
 #SBATCH -n 2
 #SBATCH -t 48:00:00
-#SBATCH -J htseq_run_comp
+#SBATCH -J htseq_run_rev_pilot
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user arnarkari.sigurearsonsandholt.9531@student.uu.se
 
@@ -12,4 +12,4 @@ module load bioinfo-tools
 module load htseq/0.9.1
 
 # Your commands
-bash scripts/htseq/htseq_strand_run.sh results/star/mapped_reads/in_vitro_complementary
+bash scripts/htseq/htseq_rev_run.sh results/star/mapped_reads/in_vitro_pilot
