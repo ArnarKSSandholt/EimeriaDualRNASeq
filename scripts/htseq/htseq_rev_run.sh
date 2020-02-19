b7debc364804d5bf19452e0b1335ff1765ef1202
Arnar Kári Sigurðarson Sandholt <arnarkari.sigurearsonsandholt.9531@student.uu.se>
2020-02-19 08:56:54 +0100

    Changed output path

--- scripts/htseq/htseq_rev_run.sh
+++ scripts/htseq/htseq_rev_run.sh
@@ -2,11 +2,11 @@
 # Usage: bash htseq_reverse_run.sh /Path/to/mapped/read/file/folder
 
 GROUP_NAME=$(echo ${1} | cut -d '/' -f 4)
-mkdir -p results/htseq/${GROUP_NAME}
+mkdir -p results/htseq/reverse/${GROUP_NAME}
 
 for f in $(ls ${1}/*.bam | sed 's/_Aligned.sortedByCoord.out.bam//' | sort -u)
 do
     OUTPUT_NAME=$(echo ${f} | rev | cut -d '/' -f 1 | rev)
     htseq-count -f bam -r pos -s reverse ${f}_Aligned.sortedByCoord.out.bam results/star/merged_reference/eimeria_chicken_merge_gene_id.gtf \
-        > results/htseq/${GROUP_NAME}/${OUTPUT_NAME}_reverse.counts
+        > results/htseq/reverse/${GROUP_NAME}/${OUTPUT_NAME}_reverse.counts
 done
