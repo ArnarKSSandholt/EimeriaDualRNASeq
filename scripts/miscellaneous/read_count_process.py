8bba40ff979362f660250be8e19ddff1067b935a
Arnar Kári Sigurðarson Sandholt <arnarkari.sigurearsonsandholt.9531@student.uu.se>
2020-02-20 10:43:59 +0100

    Added number of reads mapping to no feature and ambiguous to final table

--- scripts/miscellaneous/read_count_process.py
+++ scripts/miscellaneous/read_count_process.py
@@ -38,7 +38,9 @@ while i < len(read_count_filenames):
                 eimeria_read_num = sum(eimeria_table.gene_count)
                 chicken_read_num = sum(chicken_table.gene_count)
                 eimeria_read_perc = eimeria_read_num/total_read_num_sum
-                result_list.append([curr_name, total_read_num_sum, chicken_read_num, eimeria_read_num, eimeria_read_perc])
+                no_feature_num = stat_table.iloc[0]
+                ambiguous_num = stat_table.ilov[1]
+                result_list.append([curr_name, total_read_num_sum, chicken_read_num, eimeria_read_num, eimeria_read_perc, no_feature_num, ambiguous_num])
                 stat_table.to_csv(output_path+"/"+curr_name+"_stat_table.csv", sep = ",")
                 eimeria_table.to_csv(output_path+"/"+curr_name+"_eimeria_table.csv", sep = ",")
                 chicken_table.to_csv(output_path+"/"+curr_name+"_chicken_table.csv", sep = ",")
@@ -46,7 +48,8 @@ while i < len(read_count_filenames):
         old_name = curr_name
     i += 1
 
-header_list = ["File_name","Total_number_of_mapped_reads", "Number_of_reads_mapped_to_chicken", "Number_of_reads_mapped_to_Eimeria", "Percentage_of_Eimeria_reads"]
+header_list = ["File_name","Total_number_of_mapped_reads", "Number_of_reads_mapped_to_chicken", "Number_of_reads_mapped_to_Eimeria", \
+    "Percentage_of_Eimeria_reads", "Number_of_read_not_mapped_to_feature", "Number_of_reads_mapped_to_multiple_features"]
 data_table = pd.DataFrame(result_list, columns=header_list)
 out_table = pd.merge(metadata_table,data_table, on = "File_name")
 out_table.to_csv(output_path+"/metadata_table.csv", sep = ",", index = False)
