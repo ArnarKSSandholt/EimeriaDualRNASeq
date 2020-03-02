75f937b01f6d16ce2a8d358c9390577b22ff235d
Arnar Kári Sigurðarson Sandholt <arnarkari.sigurearsonsandholt.9531@student.uu.se>
2020-03-02 16:32:48 +0100

    Finished the DE analysis of the in vitro data

--- scripts/de_analysis/differential_expression.R
+++ scripts/de_analysis/differential_expression.R
@@ -1,6 +1,7 @@
 # A pipeline for the differential expression analysis of a dual RNA-seq dataset of chickens and Eimeria tenella parsites
 
 library(edgeR)
+library(EnhancedVolcano)
 
 # Specify the files with the count data, the path to them and the labels
 chicken_files <- c("1_S17_chicken_table.csv","2_S18_chicken_table.csv","3_S19_chicken_table.csv","4_S20_chicken_table.csv",
@@ -34,12 +35,12 @@ infection_status <- c("U","I","U","I","U","I","U","I","U","I","U","I","U","I","U
 timepoint_chicken <- c("4","4","24","24","2","2","4","4","24","24","0","4","4","24","24","48","48","72","72","0","48","48",
                        "72","72","48","48","72","72","2","2","2","2","12","12","12","12","12","12")
 timepoint_eimeria <- c("0","4","24","2","4","24","4","24","48","72","48","72","48","72","2","2","12","12","12")
-batch_chicken <- c("160308","160308","160308","160308","160314","160314","160314","160314","160314","160314",
-                   "170131","170131","170131","170131","170131","170131","170131","170131","170131","170207",
-                   "170207","170207","170207","170207","170221","170221","170221","170221","170324","170324",
-                   "170324","170324","170410","170410","170410","170410","170410","170410")
-batch_eimeria <- c("160308","160308","160314","160314","160314","170131","170131","170131","170131","170207",
-                   "170207","170221","170221","170324","170324","170410","170410","170410","170427")
+#batch_chicken <- c("160308","160308","160308","160308","160314","160314","160314","160314","160314","160314",
+#                   "170131","170131","170131","170131","170131","170131","170131","170131","170131","170207",
+#                   "170207","170207","170207","170207","170221","170221","170221","170221","170324","170324",
+#                   "170324","170324","170410","170410","170410","170410","170410","170410")
+#batch_eimeria <- c("170427","160308","160308","160314","160314","160314","170131","170131","170131","170131","170207",
+#                   "170207","170221","170221","170324","170324","170410","170410","170410")
 group_chicken <- factor(paste(infection_status,timepoint_chicken,sep="."))
 
 # Create the DGElist object containing the count data in a format that edgeR can work with
@@ -65,20 +66,36 @@ plotMDS(chicken_dgelist_filt_norm)
 plotMDS(eimeria_dgelist_filt_norm)
 
 # TODO: The chicken analysis design
-#data.frame(Sample=colnames(chicken_dgelist),infection_status,timepoint,group_chicken)
-#design <- model.matrix(~0+group_chicken)
-#rownames(design) <- colnames(chicken_dgelist)
+chicken_dgelist_filt_norm$samples$group <- relevel(chicken_dgelist_filt_norm$samples$group, ref="U.0")
+design_chicken <- model.matrix(~group, data = chicken_dgelist_filt_norm$samples)
+#design_chicken <- model.matrix(~infection_status+timepoint_chicken)
+rownames(design_chicken) <- colnames(chicken_dgelist)
 
 # Define the design of the eimeria analysis
-design_eimeria <- model.matrix(~timepoint_eimeria+batch_eimeria)
+design_eimeria <- model.matrix(~timepoint_eimeria)
 rownames(design_eimeria) <- colnames(eimeria_dgelist_filt_norm)
 
 # Estimate the dispersion for each dataset and plot for the biological coefficient of variation (BCV)
-#chicken_disp <- estimateDisp(chicken_dgelist, design, robust = TRUE)
+chicken_disp <- estimateDisp(chicken_dgelist_filt_norm, design_chicken, robust = TRUE)
 eimeria_disp <- estimateDisp(eimeria_dgelist_filt_norm, design_eimeria, robust = TRUE)
 
-#plotBCV(chicken_disp)
+plotBCV(chicken_disp)
 plotBCV(eimeria_disp)
+
+# Test for differential expression in chicken
+fit_chicken <- glmQLFit(chicken_disp, design_chicken)
+contrasts_chicken <- makeContrasts(UI.2=groupI.2-groupU.2, UI.4=groupI.4-groupU.4, UI.12=groupI.12-groupU.12, 
+                                   UI.24=groupI.24-groupU.24, UI.48=groupI.48-groupU.48, UI.72=groupI.72-groupU.72,
+                                   levels = design_chicken)
+
+qlf_chicken_I_vs_U.0 <- glmQLFTest(fit_chicken, coef = 2:7)
+qlf_chicken_U_vs_U.0 <- glmQLFTest(fit_chicken, coef = 8:13)
+qlf_chicken_UI.2 <- glmQLFTest(fit_chicken, contrast = contrasts_chicken[,"UI.2"])
+qlf_chicken_UI.4 <- glmQLFTest(fit_chicken, contrast = contrasts_chicken[,"UI.4"])
+qlf_chicken_UI.12 <- glmQLFTest(fit_chicken, contrast = contrasts_chicken[,"UI.12"])
+qlf_chicken_UI.24 <- glmQLFTest(fit_chicken, contrast = contrasts_chicken[,"UI.24"])
+qlf_chicken_UI.48 <- glmQLFTest(fit_chicken, contrast = contrasts_chicken[,"UI.48"])
+qlf_chicken_UI.72 <- glmQLFTest(fit_chicken, contrast = contrasts_chicken[,"UI.72"])
 
 # Test for differential expression in Eimeria
 fit_eimeria <- glmQLFit(eimeria_disp, design_eimeria)
@@ -91,6 +108,77 @@ qlf_eimeria_48 <- glmQLFTest(fit_eimeria, coef = 6)
 qlf_eimeria_72 <- glmQLFTest(fit_eimeria, coef = 7)
 qlf_eimeria_all <- glmQLFTest(fit_eimeria, coef = 2:7)
 qlf_eimeria_batch <- glmQLFTest(fit_eimeria, coef = 8:14)
+
+# Volcano plots for all comparisons
+EnhancedVolcano(qlf_chicken_UI.2$table,
+                lab = rownames(qlf_chicken_UI.2$table),
+                title = 'Chicken in vitro 2h',
+                x = 'logFC',
+                y = 'PValue')
+EnhancedVolcano(qlf_chicken_UI.4$table,
+                lab = rownames(qlf_chicken_UI.4$table),
+                title = 'Chicken in vitro 4h',
+                x = 'logFC',
+                y = 'PValue')
+EnhancedVolcano(qlf_chicken_UI.12$table,
+                lab = rownames(qlf_chicken_UI.12$table),
+                title = 'Chicken in vitro 12h',
+                x = 'logFC',
+                y = 'PValue')
+EnhancedVolcano(qlf_chicken_UI.24$table,
+                lab = rownames(qlf_chicken_UI.24$table),
+                title = 'Chicken in vitro 24h',
+                x = 'logFC',
+                y = 'PValue')
+EnhancedVolcano(qlf_chicken_UI.48$table,
+                lab = rownames(qlf_chicken_UI.48$table),
+                title = 'Chicken in vitro 48h',
+                x = 'logFC',
+                y = 'PValue')
+EnhancedVolcano(qlf_chicken_UI.72$table,
+                lab = rownames(qlf_chicken_UI.72$table),
+                title = 'Chicken in vitro 72h',
+                x = 'logFC',
+                y = 'PValue')
+
+EnhancedVolcano(qlf_eimeria_72$table,
+                lab = rownames(qlf_eimeria_72$table),
+                title = 'Eimeria in vitro 72h',
+                x = 'logFC',
+                y = 'PValue')
+EnhancedVolcano(qlf_eimeria_2$table,
+                lab = rownames(qlf_eimeria_2$table),
+                title = 'Eimeria in vitro 2h',
+                x = 'logFC',
+                y = 'PValue')
+EnhancedVolcano(qlf_eimeria_4$table,
+                lab = rownames(qlf_eimeria_4$table),
+                title = 'Eimeria in vitro 4h',
+                x = 'logFC',
+                y = 'PValue')
+EnhancedVolcano(qlf_eimeria_12$table,
+                lab = rownames(qlf_eimeria_12$table),
+                title = 'Eimeria in vitro 12h',
+                x = 'logFC',
+                y = 'PValue')
+EnhancedVolcano(qlf_eimeria_48$table,
+                lab = rownames(qlf_eimeria_48$table),
+                title = 'Eimeria in vitro 48h',
+                x = 'logFC',
+                y = 'PValue')
+EnhancedVolcano(qlf_eimeria_24$table,
+                lab = rownames(qlf_eimeria_24$table),
+                title = 'Eimeria in vitro 24h',
+                x = 'logFC',
+                y = 'PValue')
+
+
+
+
+
+
+
+
 
 
 
