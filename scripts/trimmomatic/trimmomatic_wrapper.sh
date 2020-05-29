cc92536181db0cf3d8a526b33b6790ecde25a960
Arnar Kári Sigurðarson Sandholt <arnarkari.sigurearsonsandholt.9531@student.uu.se>
2020-05-29 10:30:04 +0200

    Added new in vivo data to the script

--- scripts/trimmomatic/trimmomatic_wrapper.sh
+++ scripts/trimmomatic/trimmomatic_wrapper.sh
@@ -12,7 +12,9 @@ module load bioinfo-tools
 module load trimmomatic
 
 # Your commands
-bash scripts/trimmomatic/trimmomatic_run.sh data/in_vitro_pilot/161012_D00457_0163_AC9TWTANXX/Sample_*
-bash scripts/trimmomatic/trimmomatic_run.sh data/in_vitro_complementary/170830_D00457_0216_ACB7DNANXX/Sample_*
-bash scripts/trimmomatic/trimmomatic_run.sh data/SH-2259/191210_A00605_0093_BHNFNHDSXX/Sample_SH-2259-*
-bash scripts/trimmomatic/trimmomatic_run.sh data/SI-2311/191105_A00605_0082_AHGV3LDRXX/Sample_SI-2311-*
+#bash scripts/trimmomatic/trimmomatic_run.sh data/in_vitro_pilot/161012_D00457_0163_AC9TWTANXX/Sample_*
+#bash scripts/trimmomatic/trimmomatic_run.sh data/in_vitro_complementary/170830_D00457_0216_ACB7DNANXX/Sample_*
+#bash scripts/trimmomatic/trimmomatic_run.sh data/SH-2259/191210_A00605_0093_BHNFNHDSXX/Sample_SH-2259-*
+#bash scripts/trimmomatic/trimmomatic_run.sh data/SI-2311/191105_A00605_0082_AHGV3LDRXX/Sample_SI-2311-*
+bash scripts/trimmomatic/trimmomatic_run.sh data/TC_2482/200507_A00181_0173_BHLVH3DRXX/Sample_TC_2482-*
+bash scripts/trimmomatic/trimmomatic_run.sh data/TC-2483/200409_A00605_0120_BH527GDSXY/Sample_TC_2483-*
