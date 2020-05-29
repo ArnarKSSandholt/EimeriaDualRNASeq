3a46c29a639ec71ee35ae52935f91fa030846f19
Arnar Kári Sigurðarson Sandholt <arnarkari.sigurearsonsandholt.9531@student.uu.se>
2020-05-29 12:48:46 +0200

    Added in scripts for QC and mapping of the new in vivo data

---
+++ scripts/star/star_run_wrapper_TC-2482.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+
+#SBATCH -A snic2020-15-16
+#SBATCH -p core
+#SBATCH -n 8
+#SBATCH -t 48:00:00
+#SBATCH -J star_run_TC-2482
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user arnarkari.sigurearsonsandholt.9531@student.uu.se
+
+module load bioinfo-tools
+module load star/2.7.2b
+
+# Your commands
+bash scripts/star/star_run.sh results/trimmomatic/TC-2482
